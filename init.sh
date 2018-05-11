dde0b7b8b874abb9b6b750fff8a021f40cfc8e4f
sfus <sfus.dev@gmail.com>
2018-05-11 17:00:52 +0900

    Initial revision

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# -> http://blog.serverworks.co.jp/tech/2017/05/22/ansible-for-mac/
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew insatll python
+brew install ansible
