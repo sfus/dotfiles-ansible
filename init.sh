a70d34256b23abb70df89225869bdf6be048bd5a
sfus <sfus.pub@gmail.com>
2018-05-11 17:02:18 +0900

    Squash all commits to publish
    Update homebrew/homebrew-cask packages
    
    Update playbook
    
    Update playbook
    
    Update playbook
    
    Update regexp
    
    Add oh-my-zsh setting
    
    Add oh-my-zsh setting
    
    Change oh-my-zsh setting
    
    Rename playbook.yml to site.yml
    
    Split roles/homebrew-cask
    
    Split roles/homebrew
    
    Split roles/dotfiles
    
    Rename runner script name
    
    Update homebrew-cask install option setting
    
    Change dotfiles task order for oh-my-zsh
    
    Fix homebrew-cask install task
    
    Revert "Update homebrew-cask install option setting"
    
    This reverts commit b89bfc80bcc4230360ffb6b9940ae4e55443c391.
    
    Update homebrew-cask list
    
    Add tags to roles
    
    Update homebrew list
    
    Add mas settings
    
    Update run script
    
    Update mas task to skip successfully
    
    Update install packages
    
    Disable Xcode mas install
    
    Update package list
    
    Update dotfiles role
    
    Update init shell comment
    
    Update homebrew/homebrew-cask settings
    
    Add ghq to homebrew list
    
    Add wget to homebrew list
    
    Add fontforge to homebrew list
    
    Add fonts role
    
    Add java8 to homebrew-cask list
    
    Add fuse settings
    
    Add peco and reattach-to-user-namespace to homebrew list
    
    Add other homebrew packages
    
    Rename homebrew list
    
    Add new homebrew list
    
    Add update brew list task
    
    Add update brew cask list task
    
    Add sshfs to homebrew list
    
    Add xquartz to homebrew-cask list
    
    Update brewfile/caskfile
    
    Add homebrew-after-cask and curlftpfs
    
    Change fuse extension to homebrew-after-cask
    
    Add bitbar to homebrew-cask list
    
    Add README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md indent
    
    Add Google Backup README
    
    Add translate-shell
    
    Update bartender setteing
    
    Update Stay settings
    
    Add julia to homebrew-cask list
    
    Add my custom utf8proc to homebrew list
    
    Update README
    
    Add mysql client to homebrew list
    
    Change install order for dependencies
    
    Add customized tmux to homebrew list
    
    Update Mac settings
    
    Update README for calendar setting
    
    Add logitech-options to homebrew cask list
    
    Add cmigemo to homebrew list (but already installed)
    
    Update README.md
    
    Delete unused directories
    
    Delete homebrew-after-cask role
    
    Add homebrew block and tags
    
    Add ruby role
    
    Add python role
    
    Add jedi.el setup task
    
    Add Dash settings
    
    Add tmux-powerline clone task
    
    Add Bartender 3 setting
    
    Update mac settings
    
    Add exiftool to homebrew list
    
    Update install softwares
    
    Update casklist
    
    Update for fonts role
    
    Delete python2 setting
    
    Add heroku to homebrew list
    
    Update python pip install packages
    
    Update global for pygments to homebrew list
    
    Add nvm roles for Node.js
    
    Comment out npm from homebrew list to use nvm
    
    Add cordova package for npm list
    
    Change node package manager from nvm to ndenv
    
    Add ndenv global
    
    Add dotfiles-private task
    
    Update homebrew lists
    
    Add plenv settings
    
    Add cpanm and carton install
    
    Add el-get settings to dotfiles role
    
    Delete README.md for publish
    
    Add kitematic to homebrew-cask list
    
    Add mysqlworkbench to homebrew-cask list
    
    Add ninja to homebrew list
    
    Add adoptopenjdk to homebrew-cask list
    
    Add giphy to mas list
    
    Add pluntuml to homebrew list
    
    Add vlc to homebrew-cask list
    
    Add gensim
    
    Add mecab to homebrew list
    
    Add mecab-python3 to pip
    
    Fix typo
    
    Update to create el-get directory
    
    Add ansible.cfg for init
    
    Update custom el-get package symlink
    
    Update homebrew-cask for java8 with caskroom/versions
    
    Change to comment out
    
    Add kafka client into homebrew list
    
    Add jetbrains-toolbox into homebrew-cask list
    
    Add protobuf to homebrew list
    
    Add kotlin to homebrew list
    
    Add visualvm to homebrew-cask list
    
    Add Eclipse Memory Analyzer (mat) to homebrew-cask list
    
    Add drone-cli to homebrew list
    
    Delete private files

--- init.sh
+++ init.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
 # -> http://blog.serverworks.co.jp/tech/2017/05/22/ansible-for-mac/
+# -> https://techte.co/2018/01/22/ansible-mac/
+
+# Xcode command line tools
+# $ xcode-select --install
+
+# Xcode license accept
+# $ sudo xcodebuild -license accept
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew insatll python
+brew doctor
+brew install python
 brew install ansible
