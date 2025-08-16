fbf8d21027200c093a8ebd03e88a892600b0d469
sfus <sfus.dev@gmail.com>
2025-08-16 19:19:04 +0900

    Add nvim install playbook

--- run.sh
+++ run.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-ansible-playbook -i hosts site.yml $*
+# Default ansible-playbook command
+CMD="ansible-playbook -i hosts site.yml"
+
+# Check if "-t nvim" is included in the arguments, then add "-K"
+for arg in "$@"; do
+  if [ "$arg" = "-t" ]; then
+    next_is_tag=1
+  elif [ "$next_is_tag" = "1" ]; then
+    if [ "$arg" = "nvim" ]; then
+      CMD="$CMD -K"
+    fi
+    next_is_tag=0
+  fi
+done
+
+# Execute ansible-playbook with the modified command and all original arguments
+$CMD "$@"
